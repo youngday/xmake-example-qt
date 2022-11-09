cff9ccb470b60cbb40b7dbe998cf6f23add78521
youngday <young.day@aliyun.com>
2022-11-09 22:30:58 +0800

    package test

--- xmake.lua
+++ xmake.lua
@@ -1,77 +1,39 @@
+set_project("xmake-example-qt")
+set_languages("c++17")
+add_rules("mode.debug", "mode.release")
+-- cmake packages
+add_requires("cmake::LibXml2", {system = true})
+add_requires("cmake::OpenCV", {system = true})
+-- conan packages
+-- add_requires("conan::xpack 1.0.2", {alias = "xpack",debug = true})
+-- add_requires("conan::sqlite_orm 1.7.1", {alias = "sqlite_orm",debug = true})
+
+-- native xmake packages
+add_requires("tbox master", {debug = true})
+add_requires("openssl", {alias = "openssl",
+    configs = {options = "OpenSSL:shared=True"}})
+add_requires("sqlite3 3.39.0","yaml-cpp 0.7.0","fmt 9.1.0","coost 3.0.0","cppzmq 4.8.1","toml++ 3.2.0","cpr 1.8.3","concurrentqueue","xtensor","xtl","xtensor-blas") 
+
+
 add_rules("mode.debug", "mode.release")
 
 target("test")
-    add_rules("qt.quickapp")
-    add_headerfiles("src/*.h")
+    set_kind("static")
+    add_files("src/test.cpp")
+    -- add_packages("pcre2", {public = true})
+
+target("qt")
+    add_deps("test")
     add_files("src/*.cpp")
     add_files("src/qml.qrc")
+    add_includedirs("include")
+    add_headerfiles("include/*.h","include/*.hpp")
+    add_rules("qt.quickapp")
 
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro defination
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
+    
+    add_packages("cmake::LibXml2","cmake::OpenCV")
+    -- add_packages("conan::xpack","conan::sqlite_orm")  
+    add_packages("tbox","yaml-cpp","fmt","coost","cppzmq","sqlite3")
+
+    add_defines("LOGURU_WITH_STREAMS","LOGURU_USE_FMTLIB")
 
